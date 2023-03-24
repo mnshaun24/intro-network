626f8cd88488db66b60e9b427632d3351b46f362
Shaun Sticka <shaun.sticka@outlook.com>
2023-03-24 12:41:49 -0500

    theme change logic complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,54 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "pages/homePage";
 import LoginPage from "pages/loginPage";
 import ProfilePage from "pages/profilePage";
-import { createContext } from "react";
 import { useState } from "react";
-export const ThemeContext = createContext(null);
+import { ThemeContext } from "state/context";
 
 function App() {
+  // Theme change logic
+  function getLocalStorageTheme() {
+    console.log(localStorage);
+    return localStorage.getItem("theme")
+      ? localStorage.getItem("theme")
+      : "light";
+  }
 
-  const [theme, setTheme] = useState("light");
+  function setLocalStorageTheme() {
+    if (theme === "light") {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  }
+
+  const [theme, setTheme] = useState(getLocalStorageTheme);
 
   const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"))
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+    setLocalStorageTheme();
   };
 
   return (
-<ThemeContext.Provider value={{ theme, toggleTheme }}>
-    <div id={theme} className="app">
-      <label className="allText">{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-      <input type="checkbox" className="toggle" onChange={toggleTheme} checked={theme === "dark"} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme} className="app">
+        <label className="allText">
+          {theme === "light" ? "Light Mode" : "Dark Mode"}
+        </label>
+        <input
+          type="checkbox"
+          className="toggle"
+          onChange={toggleTheme}
+          checked={theme === "dark"}
+        />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </ThemeContext.Provider>
-
   );
 }
 
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  mode: "light",
   user: null,
   token: null,
   posts: [],
